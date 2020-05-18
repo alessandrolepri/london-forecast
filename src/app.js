d7bb8a84f6e94883b549756890e61e1e98bc8358
Alessandro Lepri <alex.lepri84@gmail.com>
2020-05-18 10:56:59 +0100

    Syntax

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     currentTime.innerHTML =  firstDots + today + " " + "GMT" + lastDots;
 
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=${process.env.APIKEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=${process.env.API_KEY}&units=metric`)
 
         .then(response => response.json()
         .then(data => {
